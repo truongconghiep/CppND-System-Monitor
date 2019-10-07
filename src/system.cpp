1e786fef22b0c612bba691d3857df26580608bdd
truongconghiep <truongconghiep@gmail.com>
2019-10-07 22:31:20 +0200

    implemented os version

--- src/system.cpp
+++ src/system.cpp
@@ -7,6 +7,7 @@
 #include "process.h"
 #include "processor.h"
 #include "system.h"
+#include "linux_parser.h"
 
 using std::set;
 using std::size_t;
@@ -26,7 +27,7 @@ std::string System::Kernel() { return string(); }
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return this->osName;}
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
@@ -36,3 +37,13 @@ int System::TotalProcesses() { return 0; }
 
 // TODO: Return the number of seconds since the system started running
 long int System::UpTime() { return 0; }
+
+System::System() 
+{
+  /*
+  Getting initial info about system
+  Initial data for individual cores is set
+  System data is set
+  */
+  this->osName = LinuxParser::OperatingSystem();
+}
